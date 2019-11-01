571a09f1c27f998ba28a75c128f1df9c1a0007f6
Apparition2018 <88850180@163.com>
2019-11-01 09:32:23 +0800

    2019-11-01 09:32:11

--- src/main/java/other/设计模式/creational/singleton/SingletonDemo.java
+++ src/main/java/other/设计模式/creational/singleton/SingletonDemo.java
@@ -1,5 +1,6 @@
 package other.设计模式.creational.singleton;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -25,37 +26,38 @@ import org.junit.Test;
  */
 public class SingletonDemo {
 
+    /**
+     * 测试饿汉模式
+     */
     @Test
-    public void singleton() {
-        // 饿汉模式
-        Singleton1 s1 = Singleton1.getSInstance();
-        Singleton1 s2 = Singleton1.getSInstance();
-        // 懒汉模式
-        // Singleton2 s1 = Singleton2.getSInstance();
-        // Singleton2 s2 = Singleton2.getSInstance();
-        if (s1 == s2) {
-            System.out.println("s1 和 s2 是同一个实例");
-        } else {
-            System.out.println("s1 和 s2 不是同一个实例");
-        }
+    public void testEager() {
+        Assert.assertSame(EagerSingleton.getSInstance(), EagerSingleton.getSInstance());
+    }
+
+    /**
+     * 测试懒汉模式
+     */
+    @Test
+    public void testLazy() {
+        Assert.assertSame(LazySingleton.getSInstance(), LazySingleton.getSInstance());
     }
 }
 
 /**
  * 饿汉模式，线程安全
  */
-class Singleton1 {
+class EagerSingleton {
     // 1.创建类的唯一实例，使用 private static 修饰
     // 类加载就创建唯一实例，形象成为饿汉模式
-    private static Singleton1 instance = new Singleton1();
+    private static EagerSingleton instance = new EagerSingleton();
 
     // 2.将构造方法私有化，不允许外部直接创建对象
-    private Singleton1() {
+    private EagerSingleton() {
 
     }
 
     // 3.提供一个用于获取实例的方法，使用 public static 修饰
-    public static Singleton1 getSInstance() {
+    public static EagerSingleton getSInstance() {
         return instance;
     }
 }
@@ -63,20 +65,20 @@ class Singleton1 {
 /**
  * 懒汉模式，线程不安全
  */
-class Singleton2 {
+class LazySingleton {
     // 1.创建类的唯一实例，使用 private static 修饰
-    private static Singleton2 instance;
+    private static LazySingleton instance;
 
     // 2.将构造方法私有化，不允许外部直接创建对象
-    private Singleton2() {
+    private LazySingleton() {
 
     }
 
     // 3.提供一个用于获取实例的方法，使用 public static 修饰
     // 调用方法才创建唯一实例，形象成为懒汉模式
-    public static Singleton2 getSInstance() {
+    public static LazySingleton getSInstance() {
         if (instance == null) {
-            instance = new Singleton2();
+            instance = new LazySingleton();
         }
         return instance;
     }
@@ -85,19 +87,19 @@ class Singleton2 {
 /**
  * 静态内部类，线程安全
  */
-class Singleton3 {
+class StaticInnerClassSingleton {
     // 1.将构造方法私有化，不允许外部直接创建对象
-    private Singleton3() {
+    private StaticInnerClassSingleton() {
 
     }
 
     // 2.静态内部类
     private static class SingletonHolder {
-        private static Singleton3 INSTANCE = new Singleton3();
+        private static StaticInnerClassSingleton INSTANCE = new StaticInnerClassSingleton();
     }
 
     // 3.提供一个用于获取实例的方法，使用 public static 修饰
-    public static Singleton3 getSInstance() {
+    public static StaticInnerClassSingleton getSInstance() {
         return SingletonHolder.INSTANCE;
     }
 }
