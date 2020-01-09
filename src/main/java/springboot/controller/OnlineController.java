5a28bdbf02c3ce98325e0b3d72b17373625edc7e
Apparition2018 <88850180@163.com>
2020-01-09 09:44:00 +0800

    2020-01-09 09:43:53

--- src/main/java/springboot/controller/OnlineController.java
+++ src/main/java/springboot/controller/OnlineController.java
@@ -7,12 +7,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+/**
+ * 使用 HttpSessionListener 实现统计在线人数
+ */
 @RestController
 @RequestMapping("/online")
 public class OnlineController {
 
     @RequestMapping("/test")
-    public String online(HttpServletRequest request, HttpServletResponse response) {
+    public String online(HttpServletRequest request) {
         HttpSession session = request.getSession();
         Object count = session.getServletContext().getAttribute("count");
         return "count : " + count;
--- src/main/java/springboot/listener/OnlineSessionListener.java
+++ src/main/java/springboot/listener/OnlineSessionListener.java
@@ -19,17 +19,18 @@ public class OnlineSessionListener implements HttpSessionListener {
     public void sessionCreated(HttpSessionEvent httpSessionEvent) {
         String oldSessionId = (String) httpSessionEvent.getSession().getServletContext().getAttribute("sessionId");
         String curSessionId = httpSessionEvent.getSession().getId();
-        if (!oldSessionId.equals(curSessionId)) {
+        if (!curSessionId.equals(oldSessionId)) {
             count++;
-            httpSessionEvent.getSession().getServletContext().setAttribute("count", count);
-        } else {
-            httpSessionEvent.getSession().getServletContext().setAttribute("count", count);
         }
+        httpSessionEvent.getSession().getServletContext().setAttribute("count", count);
     }
 
+    /**
+     * 关闭浏览器，web 服务器是不知道，所以并不会触发 sessionDestroyed。
+     * session 被销毁只有两个途径，一个是 session 过期，另外一个是前端调用 session.invalidate()
+     */
     @Override
     public void sessionDestroyed(HttpSessionEvent httpSessionEvent) {
-        count--;
-        httpSessionEvent.getSession().getServletContext().setAttribute("count", count);
+        httpSessionEvent.getSession().getServletContext().setAttribute("count", --count);
     }
 }
