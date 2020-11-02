7088e23cce3d05d64f4c2d158c1b16e584610610
Apparition2018 <88850180@163.com>
2020-11-02 18:12:03 +0800

    2020-11-02 18:11:00

--- src/main/java/jar/hutool/HttpDemo.java
+++ src/main/java/jar/hutool/HttpDemo.java
@@ -1,8 +1,17 @@
 package jar.hutool;
 
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.StreamProgress;
+import cn.hutool.http.HtmlUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
 import cn.hutool.http.HttpUtil;
 import l.demo.Demo;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Http
@@ -22,12 +31,108 @@ import org.junit.Test;
 public class HttpDemo extends Demo {
 
     /**
+     * HttpRequest
+     * HttpUtil 中的 get 和 post 工具方法都是 HttpRequest 对象的封装，
+     * 因此如果想更加灵活操作 Http 请求，可以使用 HttpRequest
+     * https://hutool.cn/docs/#/http/Http%E8%AF%B7%E6%B1%82-HttpRequest
+     * <p>
+     * HttpResponse
+     * https://hutool.cn/docs/#/http/Http%E5%93%8D%E5%BA%94-HttpResponse
+     */
+    @Test
+    public void testHttp() {
+
+        // Restful
+        HttpResponse response = HttpRequest.post(BAIDU_URL)
+                .body("baidu")
+                .execute();
+
+        p(response.getStatus()); // 302
+    }
+
+    /**
      * HttpUtil
+     * 此工具封装了 HttpRequest 对象常用操作。
+     * 此模块基于 JDK 的 HttpUrlConnection 封装完成，完整支持 https、代理和文件上传。
+     * https://hutool.cn/docs/#/http/Http%E5%AE%A2%E6%88%B7%E7%AB%AF%E5%B7%A5%E5%85%B7%E7%B1%BB-HttpUtil
      */
     private static class HttpUtilDemo {
+        /**
+         * 上传
+         */
         @Test
-        public void testHttpUtil() {
-            HttpUtil.post("http://localhost:3333/chinese/test", "chinese");
+        public void testUpload() {
+            Map<String, Object> paramMap = new HashMap<>();
+            // 文件上传只需将参数中的键指定为 file
+            paramMap.put("file", new File(DEMO_FILE_PATH));
+            p(HttpUtil.post(BAIDU_URL, paramMap));
+        }
+
+        /**
+         * 下载
+         */
+        @Test
+        public void testDownload() {
+            String fileUrl = "http://mirrors.sohu.com/centos/7.3.1611/isos/x86_64/CentOS-7-x86_64-DVD-1611.iso";
+            // 如果想更加灵活的将 HTTP 内容转换写出，
+            // 可以使用 HttpUtil.download(String url, OutputStream out, boolean isCloseOut[, StreamProgress streamProgress])
+            HttpUtil.downloadFile(fileUrl, FileUtil.file("E:/"), new StreamProgress() {
+                @Override
+                public void start() {
+                    p("开始下载...");
+                }
+
+                @Override
+                public void progress(long progressSize) {
+                    p("已下载：" + FileUtil.readableFileSize(progressSize));
+                }
+
+                @Override
+                public void finish() {
+                    p("下载完成...");
+                }
+            });
+        }
+    }
+
+    /**
+     * HtmlUtil
+     * https://hutool.cn/docs/#/http/HTML%E5%B7%A5%E5%85%B7%E7%B1%BB-HtmlUtil
+     */
+    private static class HtmlUtilDemo {
+
+        @Test
+        public void testHtmlUtil() {
+            String html = HttpUtil.get(BAIDU_URL);
+            
+            // filter(html)                     过滤 html 文本，防止 XSS 攻击
+            p(HtmlUtil.filter(html));
+
+            // escape(html)                     转义
+            // ' 替换为 &#039;
+            // " 替换为 &quot;
+            // & 替换为 &amp;
+            // < 替换为 &lt;
+            // > 替换为 &gt;
+            String escape = HtmlUtil.escape(html);
+            p(escape);
+
+            // unescape(escape)                 还原转义
+            p(HtmlUtil.unescape(escape));
+
+            // removeHtml(html, tagName...)     去除指定标签及其内容
+            p(HtmlUtil.removeHtmlTag(html, "html"));
+            // unwrapHtmlTag(html, tagName...)  去除指定标签保留内容
+            p(HtmlUtil.unwrapHtmlTag(html, "html"));
+            // cleanHtmlTag(html)               去除所有标签保留内容
+            p(HtmlUtil.cleanHtmlTag(html));
+            
+            // removeHtmlAttr(html, attr...)    去除指定属性
+            p(HtmlUtil.removeHtmlAttr(html, "class"));
+            // removeAllHtmlAttr(html);         去除所有属性
+            p(HtmlUtil.removeAllHtmlAttr(html));
+
+
         }
     }
 }
