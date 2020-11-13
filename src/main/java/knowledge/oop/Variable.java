30ef02bfb3b9ab77d014af0388b8b4c13c86b184
Apparition2018 <88850180@163.com>
2020-11-14 01:41:29 +0800

    2020-11-14 01:41:00

--- src/main/java/knowledge/oop/Variable.java
+++ src/main/java/knowledge/oop/Variable.java
@@ -1,9 +1,11 @@
 package knowledge.oop;
 
+import org.junit.Test;
+
 /**
  * 变量
  */
-public class Variables {
+public class Variable {
 
     /**
      * 类变量 (Class Variables)，静态变量
@@ -12,11 +14,11 @@ public class Variables {
      * 无论一个类创建了多少个对象，类只拥有类变量的一份拷贝
      * 储存在静态存储区
      * 经常被声明为常量，很少单独使用 static 声明变量
-     * 在类被访问时创建 ?，在程序结束时销毁
-     * 为了对类的使用者可见 ?，大多数类变量声明为 public 类型
+     * 在类被访问时创建，在程序结束时销毁
+     * 为了对类的使用者可见，大多数类变量声明为 public 类型
      * 数值型变量默认值是 0，布尔型默认值是 false，引用型默认值是 null
      * 变量的值可以在声明时指定，也可以在构造方法中指定，可以在静态语句块中初始化
-     * 可以通过 ClassName.VariableName 访问
+     * 可以通过 ClassName.variableName 访问
      * 被 public static final 时，一般建议使用大写字母
      */
     private static double salary = 10000.0;
@@ -51,8 +53,46 @@ public class Variables {
     }
 
     public static void main(String[] args) {
-        System.out.println(Variables.salary);       // 类变量可通过 ClassName.VariableName 访问
-        System.out.println(new Variables().name);   // 实例变量通过 ObjectReference.VariableName 访问
+        System.out.println(Variable.salary);       // 类变量可通过 ClassName.variableName 访问
+        System.out.println(new Variable().name);   // 实例变量通过 ObjectReference.variableName 访问
+    }
+
+    /**
+     * 常量
+     * 1）编译时常量
+     * 2）运行时常量
+     */
+    private static class Constant {
+
+        @Test
+        public void test() {
+            System.out.println(Constant.A.COMPILE_TIME_CONSTANT);
+        }
+
+        @Test
+        public void test2() {
+            System.out.println(Constant.A.RUN_TIME_CONSTANT);
+        }
+
+        private static class A {
+
+            static {
+                System.out.println("Class A Was Loaded !");
+            }
+
+            /**
+             * 编译时常量：
+             * 编译后的符号表中将找不到 COMPILE_TIME_CONSTANT
+             * 不依赖于类，所以访问它不会引发类的初始化
+             */
+            public static final int COMPILE_TIME_CONSTANT = 10;
+            /**
+             * 运行时常量：
+             * 依赖于类
+             */
+            public static final int RUN_TIME_CONSTANT = "test".length();
+
+        }
     }
 
 
