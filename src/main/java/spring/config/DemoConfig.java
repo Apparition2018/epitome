fcaadf15307e67213ae5589c8de1ea45f54a9ec9
Apparition2018 <88850180@163.com>
2020-11-29 18:39:38 +0800

    2020-11-29 18:39:00

---
+++ src/main/java/spring/config/DemoConfig.java
@@ -0,0 +1,36 @@
+package spring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+/**
+ * DemoConfig
+ * <p>
+ * Spring 3.1 @EnableXXX 注解：https://www.cnblogs.com/duanxz/p/4875156.html
+ * Spring 4.0 @Configuration 的使用：https://www.cnblogs.com/duanxz/p/7493276.html
+ * Spring 4.0 @PropertySource：https://www.cnblogs.com/duanxz/archive/2012/11/06/2756362.html
+ * 获取 properties 属性的几种方式：https://blog.csdn.net/lettuce_/article/details/101518075
+ *
+ * @author Arsenal
+ * created on 2020/11/29 15:55
+ */
+// @Configuration 相当于 xml 配置文件中的 <beans>
+@Configuration
+@PropertySource("classpath:jdbc.properties")
+public class DemoConfig {
+
+    private final Environment environment;
+
+    @Autowired
+    public DemoConfig(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Bean(name = "bean", initMethod = "init", destroyMethod = "destroy")
+    public spring.bean.Bean bean() {
+        return new spring.bean.Bean();
+    }
+}
---
+++ src/main/java/spring/demo/SpringConfigDemo.java
@@ -0,0 +1,38 @@
+package spring.demo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.bean.Bean;
+import spring.config.DemoConfig;
+
+/**
+ * SpringConfigDemo
+ *
+ * @author Arsenal
+ * created on 2020/11/29 16:45
+ */
+public class SpringConfigDemo {
+    AnnotationConfigApplicationContext ac;
+
+    @Before
+    public void init() {
+        ac = new AnnotationConfigApplicationContext();
+        ac.register(DemoConfig.class);
+    }
+
+    /**
+     * 生命周期
+     */
+    @Test
+    public void testLifecycle() {
+        Bean bean = ac.getBean("bean", Bean.class);
+        bean.service();
+    }
+
+    @After
+    public void destroy() {
+        ac.close();
+    }
+}
