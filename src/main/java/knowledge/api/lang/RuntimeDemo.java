f0450e7d955caf2679e63a9ddcc2b6ff2dab6963
Apparition2018 <88850180@163.com>
2020-12-01 15:29:13 +0800

    2020-12-01 15:29:00

--- src/main/java/knowledge/api/lang/RuntimeDemo.java
+++ src/main/java/knowledge/api/lang/RuntimeDemo.java
@@ -7,6 +7,9 @@ import org.junit.Test;
  * <p>
  * Process	exec(String/String[] command)                               在单独的进程中执行指定的命令
  * Process	exec(String/String[] command, String[] envp[, File dir])    在指定环境和工作目录的独立进程中执行指定的命令
+ * <p>
+ * Runtime.getRuntime().exec() 需要注意的地方：https://www.cnblogs.com/fclbky/p/6112180.html
+ * https://www.runoob.com/manual/jdk1.6/java/lang/Runtime.html
  *
  * @author ljh
  * created on 2020/12/1 13:08
