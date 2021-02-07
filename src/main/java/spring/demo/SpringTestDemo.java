c0864ecd60389cf8ab89ca00822bacfe0c297d54
Apparition2018 <88850180@163.com>
2021-02-08 00:17:04 +0800

    2021-02-08 00:17:00

--- src/main/java/spring/demo/SpringTestDemo.java
+++ src/main/java/spring/demo/SpringTestDemo.java
@@ -2,8 +2,6 @@ package spring.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import spring.config.GameServiceConfig;
 import spring.service.GameService;
@@ -16,6 +14,16 @@ import java.util.List;
  * @author ljh
  * created on 2020/12/11 9:21
  */
+
+// junit4
+//@RunWith(SpringRunner.class)
+//@ContextConfiguration(classes = {GameServiceConfig.class})
+
+// junit5，相当于上面注解作用
+//@ExtendWith(SpringExtension.class)
+//@ContextConfiguration(classes = {GameServiceConfig.class})
+
+// junit5，相当于上面注解作用
 @SpringJUnitConfig(classes = {GameServiceConfig.class})
 public class SpringTestDemo {
 
