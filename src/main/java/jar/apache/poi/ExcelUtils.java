57dfd02a6e44ff58196c7d7efb67b40dfa15c945
Apparition2018 <88850180@163.com>
2021-03-31 18:30:05 +0800

    2021-03-31 18:30:00

--- src/main/java/jar/apache/poi/ExcelUtils.java
+++ src/main/java/jar/apache/poi/ExcelUtils.java
@@ -30,39 +30,38 @@ public class ExcelUtils<T> {
     }
     
     public List<T> excel2BeanList(InputStream inputStream, Map<Integer, String> colNumAndFieldNameMap) throws IOException, IllegalAccessException, InstantiationException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException, ParseException {
-        Workbook workbook = WorkbookFactory.create(inputStream);
-        Sheet sheet = workbook.getSheetAt(0);
         List<T> list = new ArrayList<>();
-        if (null == sheet) {
-            throw new IOException("sheet 不存在");
-        }
-        for (int rowNum = 1; rowNum <= sheet.getLastRowNum(); rowNum++) {
-            Row row = sheet.getRow(rowNum);
-            if (null == row || row.getCell(0).getCellType() == CellType.BLANK) {
-                continue;
-            }
+        Workbook workbook = WorkbookFactory.create(inputStream);
+        for (int i = 0; i < workbook.getNumberOfSheets(); i++) {
+            Sheet sheet = workbook.getSheetAt(i);
+            for (int rowNum = 1; rowNum <= sheet.getLastRowNum(); rowNum++) {
+                Row row = sheet.getRow(rowNum);
+                if (null == row || row.getCell(0).getCellType() == CellType.BLANK) {
+                    continue;
+                }
 
-            T t = clazz.newInstance();
-            for (int cellNum = 0; cellNum <= row.getLastCellNum(); cellNum++) {
-                String fieldName = colNumAndFieldNameMap.get(cellNum);
-                if (null != fieldName) {
-                    Class<?> fieldType = clazz.getDeclaredField(fieldName).getType();
-                    String setterMethodName = "set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
-                    Method method = clazz.getDeclaredMethod(setterMethodName, fieldType);
-                    switch (fieldType.getSimpleName()) {
-                        case "Integer":
-                            method.invoke(t, Integer.parseInt(getValue(row.getCell(cellNum))));
-                            break;
-                        case "Date":
-                            method.invoke(t, SDF.parse(getValue(row.getCell(cellNum))));
-                            break;
-                        default:
-                            method.invoke(t, getValue(row.getCell(cellNum)));
-                            break;
+                T t = clazz.newInstance();
+                for (int cellNum = 0; cellNum <= row.getLastCellNum(); cellNum++) {
+                    String fieldName = colNumAndFieldNameMap.get(cellNum);
+                    if (null != fieldName) {
+                        Class<?> fieldType = clazz.getDeclaredField(fieldName).getType();
+                        String setterMethodName = "set" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1);
+                        Method method = clazz.getDeclaredMethod(setterMethodName, fieldType);
+                        switch (fieldType.getSimpleName()) {
+                            case "Integer":
+                                method.invoke(t, Integer.parseInt(getValue(row.getCell(cellNum))));
+                                break;
+                            case "Date":
+                                method.invoke(t, SDF.parse(getValue(row.getCell(cellNum))));
+                                break;
+                            default:
+                                method.invoke(t, getValue(row.getCell(cellNum)));
+                                break;
+                        }
                     }
                 }
+                list.add(t);
             }
-            list.add(t);
         }
         
         return list;
