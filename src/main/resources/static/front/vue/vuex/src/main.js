2cadb46d4eac50f847fe001632703069a99cc39d
Apparition2018 <88850180@163.com>
2021-04-26 23:02:35 +0800

    2021-04-26 23:02:00

--- src/main/resources/static/front/vue/vuex/src/main.js
+++ src/main/resources/static/front/vue/vuex/src/main.js
@@ -1,22 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Vuex from 'vuex'
+import router from './router'
+import store from './store'
 
 Vue.config.productionTip = false
 
-Vue.use(Vuex)
-const store = new Vuex.Store({
-    state: {
-        count: 0
-    },
-    mutations: {
-        countIncrease(state) {
-            state.count++
-        }
-    }
-})
-
 new Vue({
-    store,
-    render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
