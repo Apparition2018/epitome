c519036cbbb4676ed77a2157f389de2f829a73a7
Apparition2018 <88850180@163.com>
2021-05-11 02:55:23 +0800

    2021-05-11 02:55:00

---
+++ src/main/java/jar/jedis/DistributedLocks.java
@@ -0,0 +1,121 @@
+package jar.jedis;
+
+import org.junit.jupiter.api.Test;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.params.SetParams;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * DistributedLocks
+ *
+ * @author Arsenal
+ * created on 2021/5/11 1:47
+ */
+public class DistributedLocks {
+
+    public static final String MONEY = "MONEY";
+    public static final String MONEY_LOCK = "MONEY_LOCK";
+    public static final JedisPool JEDIS_POOL;
+
+    static {
+        JedisPoolConfig poolConfig = new JedisPoolConfig();
+        poolConfig.setMaxIdle(5);
+        poolConfig.setMaxIdle(2);
+        JEDIS_POOL = new JedisPool(poolConfig, "127.0.0.1", 6379);
+    }
+
+    /**
+     * 没有锁的时候的情况
+     */
+    private Runnable notLock() {
+        return () -> {
+            try (Jedis jedis = JEDIS_POOL.getResource()) {
+                calImpl(jedis);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+    }
+
+    private void calImpl(Jedis jedis) {
+        int money = Integer.parseInt(jedis.get(MONEY));
+        jedis.set(MONEY, String.valueOf(money - 100));
+        System.out.println("计算" + Thread.currentThread().getName());
+    }
+
+    /**
+     * 有锁的时候的情况
+     */
+    private Runnable lock() {
+        LockInitial lockInitial = new LockInitial();
+        return () -> {
+            try (Jedis jedis = JEDIS_POOL.getResource()) {
+                cal(lockInitial, jedis);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        };
+    }
+
+    private void cal(LockInitial lockInitial, Jedis jedis) {
+        if (lockInitial.tryLock(jedis, MONEY, "100", 1000L)) {
+            calImpl(jedis);
+            lockInitial.releaseLock(jedis, MONEY, "100");
+        } else {
+            cal(lockInitial, jedis);
+        }
+    }
+
+    @Test
+    public void test() throws InterruptedException {
+        try (Jedis jedis = JEDIS_POOL.getResource()) {
+            jedis.set(MONEY, "1000");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Runnable runnable = lock();
+        for (int i = 0; i < 10; i++) {
+            Thread thread = new Thread(runnable, String.valueOf(i));
+            thread.start();
+        }
+        TimeUnit.SECONDS.sleep(3);
+        try (Jedis jedis = JEDIS_POOL.getResource()) {
+            int money = Integer.parseInt(jedis.get(MONEY));
+            System.out.println(money);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private interface Lock {
+        boolean tryLock(Jedis jedis, String key, String val, Long ttl);
+
+        boolean releaseLock(Jedis jedis, String key, String val);
+    }
+
+    private static class LockInitial implements Lock {
+
+        public static Random random = new Random();
+
+        @Override
+        public boolean tryLock(Jedis jedis, String key, String val, Long ttl) {
+            SetParams setParams = new SetParams();
+            setParams.ex(ttl);
+            setParams.nx();
+            String result = jedis.set(key, val, setParams);
+            return "OK".equals(result);
+        }
+
+        @Override
+        public boolean releaseLock(Jedis jedis, String key, String val) {
+            if (val.equals(jedis.get(key))) {
+                return jedis.del(key) > 0;
+            }
+            return false;
+        }
+    }
+}
