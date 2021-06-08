ff8019cadc19916fdb55970b4c20d4600dbc7e45
Apparition2018 <88850180@163.com>
2021-06-09 00:55:54 +0800

    2021-06-09 00:55:00

--- src/main/resources/static/front/vue/vue-cli/3/axios/src/views/case/ContactList.vue
+++ src/main/resources/static/front/vue/vue-cli/3/axios/src/views/case/ContactList.vue
@@ -1,18 +1,96 @@
 <template>
   <div class="home">
-    <van-button type="default">默认按钮</van-button>
+    <!-- 联系人列表 -->
+    <van-contact-list
+        :list="list"
+        @add="onAdd"
+        @edit="onEdit"
+    />
+    <!-- 联系人编辑 -->
+    <van-popup v-model="showEdit" position="bottom">
+      <van-contact-edit
+          is-edit
+          show-set-default
+          :contact-info="editingContact"
+          set-default-label="设为默认联系人"
+          @save="onSave"
+          @delete="onDelete"
+      />
+    </van-popup>
   </div>
 </template>
 
 <script>
-import {Button} from 'vant'
+import {ContactList, Toast, ContactEdit, Popup} from 'vant'
+import axios from 'axios'
 
 export default {
   name: 'ContactList',
-  comments: {
-    [Button.name]: Button
+  components: {
+    [ContactList.name]: ContactList,
+    [ContactEdit.name]: ContactEdit,
+    [Popup.name]: Popup
+  },
+  data () {
+    return {
+      // {
+      //    id: 1,
+      //    name: '',
+      //    tel: ''
+      // }
+      list: [],
+      // instance 实例
+      instance: null,
+      // 编辑弹窗的显隐
+      showEdit: false,
+      // 正在编辑的联系人
+      editingContact: {},
+      // 控制编辑或新建
+      isEdit: false
+    }
   },
   created () {
+    this.instance = axios.create({
+      baseURL: 'http://localhost:9000/api',
+      timeout: 1000
+    })
+    this.instance.get('contactList').then(res => {
+      this.list = res.data.data
+    }).catch(err => {
+      console.log(err)
+      Toast('请求失败，请稍后重试')
+    })
+  },
+  methods: {
+    // 添加联系人
+    onAdd () {
+      this.showEdit = true
+      this.isEdit = false
+    },
+    // 编辑联系人
+    onEdit (info) {
+      this.showEdit = true
+      this.isEdit = true
+      this.editingContact = info
+    },
+    // 保存联系人
+    onSave () {
+      this.showEdit = true
+      this.isEdit = false
+
+    },
+    // 删除联系人
+    onDelete () {
+
+    }
   }
 }
 </script>
+<style scoped>
+.van-contafct-list__add {
+  z-index: 0;
+}
+.van-popup {
+  height: 100%;
+}
+</style>
