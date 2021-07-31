6c83314a459d07aefc98ee0a402ccdb472601366
Apparition2018 <88850180@163.com>
2021-08-01 04:42:18 +0800

    2021-08-01 04:42:00

--- src/main/java/jar/apache/http/HttpClientUtils.java
+++ src/main/java/jar/apache/http/HttpClientUtils.java
@@ -1,140 +1,181 @@
 package jar.apache.http;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpStatus;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.config.CookieSpecs;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URIBuilder;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.hc.client5.http.ClientProtocolException;
+import org.apache.hc.client5.http.auth.StandardAuthScheme;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.cookie.BasicCookieStore;
+import org.apache.hc.client5.http.cookie.CookieStore;
+import org.apache.hc.client5.http.cookie.StandardCookieSpec;
+import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
+import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
+import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpStatus;
+import org.apache.hc.core5.http.ParseException;
+import org.apache.hc.core5.http.config.Registry;
+import org.apache.hc.core5.http.config.RegistryBuilder;
+import org.apache.hc.core5.http.io.HttpClientResponseHandler;
+import org.apache.hc.core5.http.io.SocketConfig;
+import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.apache.hc.core5.http.message.BasicNameValuePair;
+import org.apache.hc.core5.net.URIBuilder;
+import org.apache.hc.core5.pool.PoolConcurrencyPolicy;
+import org.apache.hc.core5.pool.PoolReusePolicy;
+import org.apache.hc.core5.util.TimeValue;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 /**
  * HttpClientUtils
- * Apache HttpComponents：https://hc.apache.org/
- * HttpClientUtil 工具类：https://www.cnblogs.com/bignew/p/6715671.html
+ * HttpClient Overview：https://hc.apache.org/httpcomponents-client-5.1.x/index.html
+ * HttpClient 工具详解：http://www.mydlq.club/article/68/
  * HttpClient 设置 cookies：https://www.cnblogs.com/lixianshengfitting/p/13840123.html
  *
  * @author ljh
  * created on 2020/11/12 21:35
  */
+@Slf4j
 public abstract class HttpClientUtils {
 
-    private static final RequestConfig REQUEST_CONFIG = RequestConfig.custom()
-            // TimeOut：https://blog.csdn.net/btlas/article/details/53710854
-            // 连接请求超时：指从连接池获取连接的超时时间
-            .setConnectionRequestTimeout(5000)
-            // 连接超时：三次握手完成时间
-            .setConnectTimeout(5000)
-            // 读取数据超时：数据传输过程中数据包之间间隔的最大时间
-            .setSocketTimeout(5000)
-            // cookie 策略
-            .setCookieSpec(CookieSpecs.DEFAULT)
-            .build();
+    private static CloseableHttpClient httpClient;
 
-    private static final CloseableHttpClient CLIENT = HttpClients.custom()
-            .setDefaultRequestConfig(REQUEST_CONFIG).build();
+    private static CookieStore cookieStore;
+
+    private static BasicCredentialsProvider basicCredentialsProvider;
+
+    private static final HttpClientResponseHandler<String> responseHandler;
+
+    // HttpClient 配置
+    // https://github.com/apache/httpcomponents-client/blob/5.1.x/httpclient5/src/test/java/org/apache/hc/client5/http/examples/ClientConfiguration.java
+    static {
+        // 连接 Socket 工厂的注册表
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.INSTANCE)
+                .register("https", SSLConnectionSocketFactory.getSystemSocketFactory())
+                .build();
+
+        // 连接管理器
+        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager(
+                socketFactoryRegistry, PoolConcurrencyPolicy.STRICT, PoolReusePolicy.LIFO, TimeValue.ofMinutes(5));
+        connManager.setDefaultSocketConfig(
+                SocketConfig.custom().setSoTimeout(30, TimeUnit.SECONDS).setTcpNoDelay(true).build()
+        );
+        connManager.setValidateAfterInactivity(TimeValue.ofSeconds(10));
+        connManager.setMaxTotal(100);
+        connManager.setDefaultMaxPerRoute(10);
+        connManager.setValidateAfterInactivity(TimeValue.ofMinutes(10));
+
+        // Cookie
+        cookieStore = new BasicCookieStore();
+
+        // Credentials Provider
+        basicCredentialsProvider = new BasicCredentialsProvider();
+
+        // 全局请求配置
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setCookieSpec(StandardCookieSpec.STRICT)
+                .setExpectContinueEnabled(true)
+                .setTargetPreferredAuthSchemes(Arrays.asList(StandardAuthScheme.NTLM, StandardAuthScheme.DIGEST))
+                .setProxyPreferredAuthSchemes(Collections.singletonList(StandardAuthScheme.BASIC))
+                .setConnectTimeout(5, TimeUnit.SECONDS)
+                .setResponseTimeout(5, TimeUnit.SECONDS)
+                .setConnectionRequestTimeout(5, TimeUnit.SECONDS)
+                .build();
+
+        // HttpClient
+        httpClient = HttpClients.custom()
+                .setConnectionManager(connManager)
+                .setDefaultCookieStore(cookieStore)
+                .setDefaultCredentialsProvider(basicCredentialsProvider)
+                .setDefaultRequestConfig(requestConfig)
+                // 设置定时清理连接池中过期的连接
+                .evictExpiredConnections()
+                .evictIdleConnections(TimeValue.ofMinutes(3))
+                .build();
+
+        // JVM 停止或重启时，关闭连接池释放掉连接
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                httpClient.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));
+
+        // 响应处理
+        // https://github.com/apache/httpcomponents-client/blob/5.1.x/httpclient5/src/test/java/org/apache/hc/client5/http/examples/ClientWithResponseHandler.java
+        responseHandler = response -> {
+            int statusCode = response.getCode();
+            if (statusCode >= HttpStatus.SC_SUCCESS && statusCode < HttpStatus.SC_REDIRECTION) {
+                HttpEntity httpEntity = response.getEntity();
+                try {
+                    return httpEntity != null ? EntityUtils.toString(httpEntity, StandardCharsets.UTF_8) : null;
+                } catch (ParseException e) {
+                    throw new ClientProtocolException(e);
+                }
+            } else {
+                throw new ClientProtocolException("Unexpected response status: " + statusCode);
+            }
+        };
+    }
 
     public static void main(String[] args) throws IOException, URISyntaxException {
     }
 
     public static String doPost(String url, Map<String, String> params) throws IOException {
-        String result = "";
-
-        // 1.使用默认配置的 httpclient
-        try (CloseableHttpClient client = HttpClients.createDefault()) {
-            // 2.创建 HttpPost 请求
-            HttpPost httpPost = new HttpPost(url);
-            httpPost.setConfig(REQUEST_CONFIG);
-            if (null != params) {
-                List<NameValuePair> nameValuePairList = new ArrayList<>();
-                for (String key : params.keySet()) {
-                    nameValuePairList.add(new BasicNameValuePair(key, params.get(key)));
-                }
-                HttpEntity formEntity = new UrlEncodedFormEntity(nameValuePairList, StandardCharsets.UTF_8);
-                httpPost.setEntity(formEntity);
-            }
-
-            // 3.执行请求，获取响应
-            try (CloseableHttpResponse response = client.execute(httpPost)) {
-                // 4.获取响应的实体内容
-                if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                    HttpEntity httpEntity = response.getEntity();
-                    result = EntityUtils.toString(httpEntity, StandardCharsets.UTF_8);
-                    EntityUtils.consume(httpEntity);
-                }
-            }
+        String responseBody;
+        // 2.创建 HttpPost 请求
+        HttpPost httpPost = new HttpPost(url);
+        if (params != null) {
+            List<BasicNameValuePair> nameValuePairList = params.entrySet().stream().map(
+                    entry -> new BasicNameValuePair(entry.getKey(), entry.getValue())).collect(Collectors.toList());
+            HttpEntity formEntity = new UrlEncodedFormEntity(nameValuePairList, StandardCharsets.UTF_8);
+            httpPost.setEntity(formEntity);
         }
+        responseBody = httpClient.execute(httpPost, responseHandler);
+        return responseBody;
+    }
 
-        return result;
+    public static String doPost(String url, String json) throws IOException {
+        String responseBody;
+        StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setEntity(entity);
+        responseBody = httpClient.execute(httpPost, responseHandler);
+        return responseBody;
     }
 
     public static String doPost(String url) throws IOException {
         return doPost(url, (Map<String, String>) null);
     }
 
-    public static String doPost(String url, String json) throws IOException {
-        String result = "";
-
-        try (CloseableHttpClient client = HttpClients.createDefault()) {
-            StringEntity entity = new StringEntity(json, ContentType.APPLICATION_JSON);
-            HttpPost httpPost = new HttpPost(url);
-            httpPost.setConfig(REQUEST_CONFIG);
-            httpPost.setEntity(entity);
-            try (CloseableHttpResponse response = client.execute(httpPost)) {
-                if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                    HttpEntity httpEntity = response.getEntity();
-                    result = EntityUtils.toString(httpEntity, StandardCharsets.UTF_8);
-                    EntityUtils.consume(httpEntity);
-                }
-            }
-        }
-
-        return result;
-    }
-
     public static String doGet(String url, Map<String, String> params) throws IOException, URISyntaxException {
-        String result = "";
-
-        // 1.使用默认配置的 httpclient
-        try (CloseableHttpClient client = HttpClients.createDefault()) {
-            // 2.创建 HttpGet 请求
-            URIBuilder builder = new URIBuilder(url);
-            if (null != params) {
-                for (String key : params.keySet()) {
-                    builder.addParameter(key, params.get(key));
-                }
-            }
-            HttpGet httpGet = new HttpGet(builder.build());
-            httpGet.setConfig(REQUEST_CONFIG);
-
-            // 3.执行请求，获取响应
-            try (CloseableHttpResponse response = client.execute(httpGet)) {
-                // 4.获取响应的实体内容
-                if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                    HttpEntity httpEntity = response.getEntity();
-                    result = EntityUtils.toString(httpEntity, StandardCharsets.UTF_8);
-                    EntityUtils.consume(httpEntity);
-                }
-            }
+        String responseBody;
+        URIBuilder builder = new URIBuilder(url);
+        if (params != null) {
+            params.forEach(builder::addParameter);
         }
-
-        return result;
+        HttpGet httpGet = new HttpGet(builder.build());
+        responseBody = httpClient.execute(httpGet, responseHandler);
+        return responseBody;
     }
 
     public static String doGet(String url) throws IOException, URISyntaxException {
