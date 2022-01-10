ac9ae32cc19747d60aa3f44fd35484763a032e54
Apparition2018 <88850180@163.com>
2022-01-11 02:39:13 +0800

    2022-01-11 02:39:00

--- src/main/java/knowledge/design/DesignPatternPrinciples.java
+++ src/main/java/knowledge/design/DesignPatternPrinciples.java
@@ -1,17 +1,16 @@
 package knowledge.design;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 /**
- * 六大原则：
- * 1.单一职责原则 (Single Responsibility Principle)
- * 2.开闭原则 (Open-Closed Principle)
- * 3.里氏替换原则 (Liskov Substitution Principle)
- * 4.依赖倒置原则 (Dependence Inversion Principle)
+ * 七大原则：
+ * 1.开闭原则 (Open Closed Principle)
+ * 2.里氏替换原则 (Liskov Substitution Principle)
+ * 3.依赖倒置原则 (Dependence Inversion Principle)
+ * 4.单一职责原则 (Single Responsibility Principle)
  * 5.接口隔离原则 (Interface Segregation Principle)
- * 6.迪米特法则 (Law of Demeter)
+ * 6.迪米特法则 (Law of Demeter) / 最少知识原则 (Least Knowledge Principle)
+ * 7.合成复用原则 (Composite Reuse Principle)
  *
  * @author ljh
  * created on 2020/9/24 16:13
@@ -19,61 +18,21 @@ import lombok.Setter;
 public class DesignPatternPrinciples {
 
     /**
-     * 单一职责原则
-     * 有且仅有一个原因引起类/方法的变更；一个类只负责一项职责
-     * http://c.biancheng.net/view/1327.html
-     */
-    static class SRP {
-
-        interface Shopping {
-            void shopping();
-        }
-
-        interface Cooking {
-            void cooking();
-        }
-
-        interface Washing {
-            void washing();
-        }
-
-        static class Jack implements Shopping {
-
-            @Override
-            public void shopping() {
-            }
-        }
-
-        static class Mary implements Cooking, Washing {
-
-            @Override
-            public void cooking() {
-            }
-
-            @Override
-            public void washing() {
-            }
-        }
-    }
-
-    /**
      * 开闭原则
-     * 对扩展开放，对修改关闭；尽量在不修改原有代码的情况下进行扩展
-     * http://c.biancheng.net/view/1322.html
+     * 对扩展开放，对修改关闭
+     * 核心思想：抽象
      */
     static class OCP {
-        private abstract static class Chart {
-            public void display() {
-
-            }
+        interface Chart {
+            public void display();
         }
 
-        static class PieChart extends Chart {
+        static class PieChart implements Chart {
             public void display() {
             }
         }
 
-        static class BarChart extends Chart {
+        static class BarChart implements Chart {
             public void display() {
             }
         }
@@ -94,33 +53,28 @@ public class DesignPatternPrinciples {
     /**
      * 里氏替换原则
      * 继承必须确保超类所拥有的性质在子类中仍然成立
-     * 所有引用基类的地方必须能透明地使用其子类的对象
-     * http://c.biancheng.net/view/1324.html
+     * 子类继承父类尽量不要重写父类方法
      */
     static class LSP {
-
-        interface EmailSender {
-            void send(Customer c);
+        interface Person {
+            void move();
         }
 
-        @Getter
-        @Setter
-        private abstract static class Customer implements EmailSender {
-            protected String name;
-            protected String email;
-        }
-
-        static class VIPCustomer extends Customer {
+        static class Adult implements Person {
 
             @Override
-            public void send(Customer c) {
+            public void move() {
+                System.out.println("步行");
             }
         }
 
-        static class CommonCustomer extends Customer {
+        static class Man extends Adult {
+        }
 
+        static class Baby implements Person {
             @Override
-            public void send(Customer c) {
+            public void move() {
+                System.out.println("爬行");
             }
         }
     }
@@ -129,65 +83,47 @@ public class DesignPatternPrinciples {
      * 依赖倒置原则
      * 高层模块不应该依赖低层模块，两者都应该依赖其抽象；抽象不应该依赖细节，细节应该依赖抽象
      * 要面向接口编程，不要面向实现编程
-     * http://c.biancheng.net/view/1326.html
      */
     static class DIP {
-        interface ICar {
-            void run();
-
-            void turn();
-
-            void stop();
+        interface OnlineStore {
+            void sell();
         }
 
-        static class BMW implements ICar {
-            public void run() {
-            }
-
-            public void turn() {
-            }
-
-            public void stop() {
+        static class BookStore implements OnlineStore {
+            @Override
+            public void sell() {
+                System.out.println("卖书");
             }
         }
 
-        static class Ford implements ICar {
-            public void run() {
-            }
-
-            public void turn() {
-            }
-
-            public void stop() {
+        static class ToyStore implements OnlineStore {
+            @Override
+            public void sell() {
+                System.out.println("卖玩具");
             }
         }
 
-        static class AutoSystem {
-            private final ICar icar;
-
-            public AutoSystem(ICar icar) {
-                this.icar = icar;
-            }
-
-            public void Run() {
-                icar.run();
-            }
-
-            public void Turn() {
-                icar.turn();
-            }
-
-            public void Stop() {
-                icar.stop();
+        static class Customer {
+            public void onlineShopping(OnlineStore store) {
+                store.sell();
             }
         }
+
+    }
+
+    /**
+     * 单一职责原则
+     * 一个类/方法应该有且仅有一个引起它变化的原因，否则类/方法应该被拆分
+     */
+    static class SRP {
     }
 
     /**
      * 接口隔离原则
      * 客户端不应该被迫依赖于它不使用的方法；一个类对另一个类的依赖应该建立在最小的接口上
      * 使用多个专门的接口比使用单一的总接口要好
-     * http://c.biancheng.net/view/1330.html
+     * SRP: 对类的约束
+     * ISP: 对接口的约束
      */
     static class ISP {
         interface Work {
@@ -198,12 +134,11 @@ public class DesignPatternPrinciples {
             void sleep();
         }
 
-        interface Managed {
+        interface BeManaged {
             void beManaged();
         }
 
-        static class Worker implements Work, Sleep, Managed {
-
+        static class Worker implements Work, Sleep, BeManaged {
             @Override
             public void work() {
             }
@@ -217,8 +152,7 @@ public class DesignPatternPrinciples {
             }
         }
 
-        static class Robot implements Work, Managed {
-
+        static class Robot implements Work, BeManaged {
             @Override
             public void work() {
             }
@@ -227,19 +161,12 @@ public class DesignPatternPrinciples {
             public void beManaged() {
             }
         }
-
-        static class Captain {
-            public void manage(Managed managed) {
-                managed.beManaged();
-            }
-        }
     }
 
     /**
-     * 迪米特法则
+     * 迪米特法则（最少知识原则）
      * 只与你的直接朋友交谈，不跟陌生人说话
-     * 一个软件实体应当尽可能少地与其他实体发生相互作用
-     * http://c.biancheng.net/view/1331.html
+     * 一个软件实体应当尽可能少地与其他软件实体发生相互作用
      */
     static class LOD {
         @Data
@@ -271,6 +198,15 @@ public class DesignPatternPrinciples {
         static class Company {
             private String name;
         }
+    }
+
+    /**
+     * 合成复用原则（组合/聚合复用原则）
+     * 在软件复用时，要尽量先使用组合或者聚合等关联关系来实现，其次才考虑使用继承关系来实现
+     * 如果要使用继承关系，则必须严格遵循里氏替换原则
+     */
+    static class CRP {
+
     }
 
 }
